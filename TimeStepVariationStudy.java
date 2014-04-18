387635e227d2e4eaf544e1f977a7bdc6f2b8297b
cj8q5 <cj8q5@mail.missouri.edu>
2014-04-18 14:07:38 -0500

    FirstCommit

---
+++ TimeStepVariationStudy.java
@@ -0,0 +1,111 @@
+package myStarJavaMacros;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
+import star.common.ImplicitUnsteadyModel;
+import star.common.PhysicsContinuum;
+import star.common.Simulation;
+import star.common.StarMacro;
+import star.common.SteadyModel;
+import starClasses.ContiuumBuilder;
+import starClasses.ImportCAE;
+import starClasses.MeshMorpher;
+import starClasses.SolversNode;
+import starClasses.StoppingCriteria;
+
+public class TimeStepVariationStudy extends StarMacro 
+{
+	double[] m_timeStep = new double[1];
+	Simulation activeSim;
+	public void execute() 
+	{
+		String currentDirectory = System.getProperty("user.dir");
+		activeSim = getActiveSimulation();
+		this.readTimeStepFile(currentDirectory + File.separator + "TimeSteps.txt");
+		
+		for (int i = 0; i < m_timeStep.length; i++)
+		{
+			// Star-CCM+ settings and variables
+			double timeStep = m_timeStep[i];
+			/**-----------------------------------------------------------------------------------------------------------------------------------------------------
+				IMPORTED PARTS NODE */
+			// Importing Abaqus plate part and its initial deflection solution
+			String fileLocation = currentDirectory + File.separator;
+			String abqFileName = "DeflectedPlate";
+			String[] regionDeflectionMapping = {"Fluid", "Fluid", "Fluid", "Fluid"};
+			String[] boundaryDeflectionMapping = {"Fluid.FSI_Front", "Fluid.FSI_Back", "Fluid.FSI_Top", "Fluid.FSI_Bottom"};
+			ImportCAE cae = new ImportCAE(activeSim, fileLocation, "Fluid");
+			cae.importAbaqusInputFile(abqFileName, false, true);
+			cae.importAbaqusOdbFile(abqFileName, "SS_Deflection");
+			cae.deformImportedAbaqusModel();
+			cae.mapAbaqusDeflectionData(regionDeflectionMapping, boundaryDeflectionMapping, "Plate.FSI_INTERFACE");
+				
+			/**-----------------------------------------------------------------------------------------------------------------------------------------------------
+				SOLVERS NODE */
+			SolversNode solvers = new SolversNode(activeSim);
+			solvers.setKepsilonRelax(0.6);
+			solvers.setUnsteadyTimeStep(timeStep, 1);
+			
+			// Setting up the mesh morpher solver
+			MeshMorpher morpher =  new MeshMorpher(activeSim, "Fluid");
+			morpher.addRegionBoundary("Fluid.FSI_Front", "SS");
+			morpher.addRegionBoundary("Fluid.FSI_Back", "SS");
+			morpher.addRegionBoundary("Fluid.FSI_Top", "SS");
+			morpher.addRegionBoundary("Fluid.FSI_Bottom", "SS");
+			
+			/**-----------------------------------------------------------------------------------------------------------------------------------------------------
+				RUNNING THE SIMULATION */
+			activeSim.getSimulationIterator().run(500);
+			activeSim.saveState(currentDirectory + File.separator + "SS_Deflection_TS_" + Double.toString(timeStep));
+			activeSim.close();
+		}
+	}
+	
+	private void readTimeStepFile(String file2Read)
+	{
+		FileReader file;
+		try 
+		{
+			file = new FileReader(file2Read);
+			BufferedReader reader = new BufferedReader(file);
+			
+			String line;
+			int i = 0;
+			while((line=reader.readLine()) != null)
+			{
+				if(line.startsWith("#"))
+				{
+					activeSim.println("test");
+					continue;
+				}
+				else
+				{
+					 activeSim.println(line);
+					 m_timeStep[i] = Double.parseDouble(line);
+				}
+				i = i++;
+			}
+			reader.close();
+		} 
+		catch (FileNotFoundException e) 
+		{
+			JOptionPane.showMessageDialog(null, e.toString());
+		} 
+		catch (NumberFormatException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		catch (IOException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
